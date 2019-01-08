c3cd78b4ab151c579c91ce4e66a7cd91df9ca5e5
magzoaa <magzoaa@gmail.com>
2019-01-07 22:05:50 -0300

    Version sin bower

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,4 @@
-angular.module("menu", ["ui.router"]);
+angular.module("menu", ["ui.router"]).config(function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
---
+++ public/js/config/configValue.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+angular.module("menu").value("config", {
+  baseUrl: "http://192.168.88.189:8000/"
+
+});
